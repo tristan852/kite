da78b77993da1bf1ec284e16506742dd5303e8fa
Tristan <tristan.tempelmann@tu-dortmund.de>
2025-07-18 22:52:26 +0200

    Rework publishing process

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
-    application
-    signing
-    
-    `java-library`
-    `maven-publish`
+    id("java")
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-group = "net.kite"
+group = "io.github.tristan852"
 version = "1.0.0"
 
 repositories {
@@ -18,66 +15,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 tasks.javadoc {
     exclude("net/kite/board/**")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            
-            pom {
-                name.set("Kite")
-                description.set("A connect four solver")
-                url.set("https://github.com/tristan852/kite")
-                
-                licenses {
-                    license {
-                        name.set("GNU General Public License version 3")
-                        url.set("https://opensource.org/license/gpl-3-0")
-                    }
-                }
-                
-                developers {
-                    developer {
-                        id.set("tristan852")
-                        name.set("Tristan")
-                        email.set("tristan84502@gmail.com")
-                    }
-                }
-                
-                scm {
-                    connection.set("scm:git:git://github.com/tristan852/kite.git")
-                    developerConnection.set("scm:git:ssh://github.com:tristan852/kite.git")
-                    url.set("https://github.com/tristan852/kite")
-                }
-            }
-        }
-    }
-    
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = findProperty("ossrhUsername") as String
-                password = findProperty("ossrhPassword") as String
-            }
-        }
-    }
-}
-
-signing {
-    useGpgCmd()
-    sign(publishing.publications["mavenJava"])
 }
