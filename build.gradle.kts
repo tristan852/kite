eb0a2289f258637eb12e7054fe83c8e9b542a6a5
Tristan <tristan.tempelmann@tu-dortmund.de>
2025-07-18 19:32:26 +0200

    Add maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("java")
     application
+    
+    signing
+    `maven-publish`
 }
 
 group = "net.kite"
@@ -20,4 +23,55 @@ tasks.jar {
             "Main-Class" to application.mainClass.get()
         )
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            
+            pom {
+                name.set("Kite")
+                description.set("A connect four solver")
+                url.set("https://github.com/tristan852/kite")
+                
+                licenses {
+                    license {
+                        name.set("GNU General Public License version 3")
+                        url.set("https://opensource.org/license/gpl-3-0")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("tristan852")
+                        name.set("Tristan")
+                        email.set("tristan84502@gmail.com")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:git://github.com/tristan852/kite.git")
+                    developerConnection.set("scm:git:ssh://github.com:tristan852/kite.git")
+                    url.set("https://github.com/tristan852/kite")
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = findProperty("ossrhUsername") as String
+                password = findProperty("ossrhPassword") as String
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
