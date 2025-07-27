014bef377aaac616138b6050bfdb8f346a6e324f
Tristan <tristan.tempelmann@tu-dortmund.de>
2025-07-27 22:59:00 +0200

    Update KiteDemo.java

--- src/main/java/net/kite/demo/KiteDemo.java
+++ src/main/java/net/kite/demo/KiteDemo.java
@@ -1,9 +1,11 @@
 package net.kite.demo;
 
+import net.kite.Kite;
+import net.kite.board.score.cache.opening.OpeningBoardScoreCaches;
 import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.dom.html.HTMLDocument;
 import org.teavm.jso.typedarrays.ArrayBuffer;
-import org.teavm.jso.typedarrays.Uint8Array;
+import org.teavm.jso.typedarrays.Int8Array;
 
 import java.io.ByteArrayInputStream;
 
@@ -11,8 +13,6 @@ public class KiteDemo {
 	
 	public void onStart() {
 		HTMLDocument doc = HTMLDocument.current();
-		
-		// call ensure... inside the onload
 		
 		XMLHttpRequest xhr = new XMLHttpRequest();
 		xhr.open("GET", "WEB-INF/classes/board_score_caches/opening.cfc");
@@ -25,29 +25,33 @@ public class KiteDemo {
 			}
 			
 			ArrayBuffer arrayBuffer = (ArrayBuffer) xhr.getResponse();
-			Uint8Array array = new Uint8Array(arrayBuffer);
+			Int8Array array = new Int8Array(arrayBuffer);
 			
-			int n = array.getByteLength();
+			int n = array.getLength();
 			byte[] bytes = new byte[n];
 			
 			for(int i = 0; i < n; i++) {
 				
-				bytes[i] = (byte) array.get(i);
+				bytes[i] = array.get(i);
 			}
 			
 			ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
 			
+			OpeningBoardScoreCaches.ensureDefaultIsLoaded(inputStream);
+			
+			System.out.println(Kite.createInstance().optimalMove());
 			System.out.println(bytes.length);
+			
+			var div = doc.createElement("div");
+			div.appendChild(doc.createTextNode("Hello world!"));
+			doc.getBody().appendChild(div);
+			
+			var div2 = doc.createElement("div");
+			div2.appendChild(doc.createTextNode("TeaVM generated element3; solver move: "));
+			doc.getBody().appendChild(div2);
 		});
+		
 		xhr.send();
-		
-		var div = doc.createElement("div");
-		div.appendChild(doc.createTextNode("Hello world!"));
-		doc.getBody().appendChild(div);
-		
-		var div2 = doc.createElement("div");
-		div2.appendChild(doc.createTextNode("TeaVM generated element3; solver move: "));
-		doc.getBody().appendChild(div2);
 	}
 	
 }
