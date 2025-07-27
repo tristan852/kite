bc2438050ad894c9cc2f646fd2d7d4c70218072a
Tristan <tristan.tempelmann@tu-dortmund.de>
2025-07-27 19:43:57 +0200

    Update KiteDemo.java

--- src/main/java/net/kite/demo/KiteDemo.java
+++ src/main/java/net/kite/demo/KiteDemo.java
@@ -1,5 +1,6 @@
 package net.kite.demo;
 
+import net.kite.Kite;
 import org.teavm.jso.dom.html.HTMLDocument;
 
 public class KiteDemo {
@@ -7,8 +8,10 @@ public class KiteDemo {
 	public void onStart() {
 		HTMLDocument doc = HTMLDocument.current();
 		
+		Kite solver = Kite.createInstance();
+		
 		var div = doc.createElement("div");
-		div.appendChild(doc.createTextNode("TeaVM generated element"));
+		div.appendChild(doc.createTextNode("TeaVM generated element" + solver.toString()));
 		doc.getBody().appendChild(div);
 	}
 	
