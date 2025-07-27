9e15403fedc68f8573f57b90fa0943e3525b1aa4
Tristan <tristan.tempelmann@tu-dortmund.de>
2025-07-27 22:00:18 +0200

    Update KiteDemo.java

--- src/main/java/net/kite/demo/KiteDemo.java
+++ src/main/java/net/kite/demo/KiteDemo.java
@@ -1,16 +1,38 @@
 package net.kite.demo;
 
-import net.kite.Kite;
+import org.teavm.jso.ajax.XMLHttpRequest;
 import org.teavm.jso.dom.html.HTMLDocument;
+import org.teavm.jso.typedarrays.ArrayBuffer;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class KiteDemo {
 	
 	public void onStart() {
 		HTMLDocument doc = HTMLDocument.current();
 		
+		int a;
+		
+		XMLHttpRequest xhr = new XMLHttpRequest();
+		xhr.open("GET", "/WEB-INF/classes/score_caches/opening.cfc");
+		xhr.setResponseType("arraybuffer");
+		xhr.onLoad(() -> {
+			if (xhr.getStatus() == 200) {
+				System.out.println("GOT IT");
+			} else {
+				
+			}
+		});
+		xhr.send();
+		
 		var div = doc.createElement("div");
-		div.appendChild(doc.createTextNode("TeaVM generated element3; solver move: " + Kite.createInstance()));
+		div.appendChild(doc.createTextNode("Hello world!"));
 		doc.getBody().appendChild(div);
+		
+		var div2 = doc.createElement("div");
+		div2.appendChild(doc.createTextNode("TeaVM generated element3; solver move: " + a));
+		doc.getBody().appendChild(div2);
 	}
 	
 }
