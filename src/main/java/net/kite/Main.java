ff6e6f79974594bf3899f756d47e574bcfc574dd
Tristan <tristan.tempelmann@tu-dortmund.de>
2025-07-29 15:45:06 +0200

    Update Main.java

--- src/main/java/net/kite/Main.java
+++ src/main/java/net/kite/Main.java
@@ -1,12 +1,10 @@
 package net.kite;
 
-import net.kite.demo.KiteDemo;
-
 public class Main {
 	
 	public static void main(String[] args) {
-		KiteDemo kiteDemo = new KiteDemo();
-		kiteDemo.onStart();
+//		KiteDemo kiteDemo = new KiteDemo();
+//		kiteDemo.onStart();
 	}
 	
 }
