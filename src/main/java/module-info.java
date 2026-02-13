57c35966c81c62c6ea1baccd1bb7e96aa1e8e253
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-13 13:13:58 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module kite.main {
 	exports net.kite.api;
+	exports net.kite.api.board.line;
+	exports net.kite.api.board.outcome;
+	exports net.kite.api.board.player.color;
+	exports net.kite.api.skill.level;
+	
 	requires org.teavm.jso.apis;
 }
