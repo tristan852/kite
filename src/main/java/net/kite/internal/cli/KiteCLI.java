3b7840b678ca680e06c8372cfb79ba43d229b526
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-14 20:08:08 +0100

    Update to cli

--- src/main/java/net/kite/internal/cli/KiteCLI.java
+++ src/main/java/net/kite/internal/cli/KiteCLI.java
@@ -1,9 +1,32 @@
 package net.kite.internal.cli;
 
+import net.kite.api.Kite;
+
+import java.util.Scanner;
+
 public class KiteCLI {
 	
 	public void onStart() {
-		System.out.println("test");
+		Kite solver = Kite.createInstance();
+		Scanner scanner = new Scanner(System.in);
+		
+		String name = Kite.getName();
+		String version = Kite.getVersion();
+		String author = Kite.getAuthor();
+		
+		String message = String.format("%s v%s by %s", name, version, author);
+		System.out.println(message);
+		System.out.println(solver);
+		
+//		while(true) {
+//			
+//			message = scanner.nextLine();
+//		}
+		
+		// TODO run help to get help message
+		
+		// commands for all solver methods and run benchmark (with and without metrics)
+		// exit command and help command
 	}
 	
 }
