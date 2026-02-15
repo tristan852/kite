68da067bf8a5ee0d34f3b285b0340a9221e485b4
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-15 23:36:10 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ graalvmNative {
             buildArgs.add("--no-fallback")
             buildArgs.add("--no-server")
             buildArgs.add("-H:+ReportExceptionStackTraces")
-            buildArgs.add("-H:-EnableGsonFeature")
+            buildArgs.add("--thin")
         }
     }
 }
