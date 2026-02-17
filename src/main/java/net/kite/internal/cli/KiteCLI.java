36d71960912df45e389fccafee4c4e66353bcb21
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-17 16:42:07 +0100

    Update KiteCLI.java

--- src/main/java/net/kite/internal/cli/KiteCLI.java
+++ src/main/java/net/kite/internal/cli/KiteCLI.java
@@ -10,36 +10,67 @@ import java.util.Scanner;
 public class KiteCLI {
 	
 	private static final char COMMAND_ARGUMENT_SEPARATOR_CHARACTER = ' ';
-	private static final String COMMAND_ARGUMENT_SEPARATOR_STRING = " ";
+	private static final String COMMAND_ARGUMENT_SEPARATOR_REGEX = "\\s+";
+	
+	private static final String VERSION_PROGRAM_ARGUMENT = "--version";
+	private static final String QUIET_PROGRAM_ARGUMENT = "--quiet";
 	
 	public void onStart(String[] programArguments) {
-		String version = Kite.getVersion();
+		boolean quiet = System.console() == null;
 		
 		if(programArguments.length != 0) {
+			if(programArguments.length != 1) {
+				
+				System.err.println("Too many program arguments!");
+				return;
+			}
 			
-			System.out.println(version);
-			return;
+			String argument = programArguments[0];
+			if(argument.equals(VERSION_PROGRAM_ARGUMENT)) {
+				
+				String version = Kite.getVersion();
+				System.out.println(version);
+				
+				return;
+			}
+			
+			if(argument.equals(QUIET_PROGRAM_ARGUMENT)) quiet = true;
 		}
 		
 		Kite solver = Kite.createInstance();
 		Scanner scanner = new Scanner(System.in);
 		
-		Runtime runtime = Runtime.getRuntime();
-		Thread shutdownThread = new Thread(() -> System.out.println("\nExiting Kite..."));
-		
-		runtime.addShutdownHook(shutdownThread);
-		
-		String name = Kite.getName();
-		String author = Kite.getAuthor();
-		
-		String message = String.format("%s v%s by %s%n%nEnter 'help' to get a list of available commands.%nAlso, please be careful as there is very little input sanitization!%n", name, version, author);
-		System.out.println(message);
+		if(!quiet) {
+			
+			Runtime runtime = Runtime.getRuntime();
+			Thread shutdownThread = new Thread(() -> System.out.println("\nExiting Kite..."));
+			
+			runtime.addShutdownHook(shutdownThread);
+			
+			String name = Kite.getName();
+			String version = Kite.getVersion();
+			String author = Kite.getAuthor();
+			
+			String message = String.format(
+					" __  __    __    ______   ______   %n/\\ \\/ /   /\\ \\  /\\__  _\\ /\\  ___\\  %n\\ \\  _\"-. \\ \\ \\ \\/_/\\ \\/ \\ \\  __\\  %n \\ \\_\\ \\_\\ \\ \\_\\   \\ \\_\\  \\ \\_____\\%n  \\/_/\\/_/  \\/_/    \\/_/   \\/_____/%n%n%s v%s by %s%n%nEnter 'help' to get a list of available commands.%n",
+					name,
+					version,
+					author
+			);
+			
+			System.out.println(message);
+		}
 		
 		while(true) {
+			
+			String message;
 			
 			try {
 				
 				message = scanner.nextLine();
+				message = message.trim();
+				
+				if(message.isBlank()) continue;
 				
 			} catch(NoSuchElementException exception) {
 				
@@ -62,8 +93,10 @@ public class KiteCLI {
 				
 				message = message.substring(i + 1);
 				
-				commandArguments = message.split(COMMAND_ARGUMENT_SEPARATOR_STRING);
+				commandArguments = message.split(COMMAND_ARGUMENT_SEPARATOR_REGEX);
 			}
+			
+			commandName = commandName.toLowerCase();
 			
 			Command command = Commands.command(commandName);
 			if(command == null) {
@@ -73,7 +106,7 @@ public class KiteCLI {
 			}
 			
 			boolean exit = command.execute(commandArguments, solver);
-			if(exit) break;
+			if(exit) return;
 		}
 	}
 	
