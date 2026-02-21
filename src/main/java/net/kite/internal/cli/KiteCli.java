6c1402c83fabf5582f3574a23a8aa2928abaf429
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-21 10:43:04 +0100

    Update KiteCli.java

--- src/main/java/net/kite/internal/cli/KiteCli.java
+++ src/main/java/net/kite/internal/cli/KiteCli.java
@@ -27,6 +27,8 @@ public final class KiteCli {
 	private static final String GAME_PROGRAM_ARGUMENT = "--game";
 	
 	public void onStart(String[] programArguments) {
+		System.out.println(System.out.charset());
+		
 		boolean quiet = System.console() == null;
 		boolean verbose = false;
 		
