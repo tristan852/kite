0c724e2441e21dcb897b6b339bf45f320d896410
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-23 12:33:51 +0100

    Update Main.java

--- src/main/java/net/kite/internal/demo/Main.java
+++ src/main/java/net/kite/internal/demo/Main.java
@@ -1,8 +1,13 @@
 package net.kite.internal.demo;
 
+import org.fusesource.jansi.Ansi;
+
 public final class Main {
 	
 	public static void main(String[] programArguments) {
+		System.out.println(Ansi.ansi().getClass());
+		System.out.println(Main.class.getResourceAsStream("benchmarks/endgame_easy.txt"));
+		
 		KiteDemo kiteDemo = new KiteDemo();
 		kiteDemo.onStart();
 	}
