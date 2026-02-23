5c6a89d70725a38fcd81e9d63457c1eb76f2f570
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-23 12:41:39 +0100

    Update Main.java

--- src/main/java/net/kite/internal/demo/Main.java
+++ src/main/java/net/kite/internal/demo/Main.java
@@ -6,7 +6,7 @@ public final class Main {
 	
 	public static void main(String[] programArguments) {
 		System.out.println(Ansi.ansi().getClass());
-		System.out.println(Main.class.getResourceAsStream("benchmarks/endgame_easy.txt"));
+		System.out.println(Main.class.getResourceAsStream("/benchmarks/endgame_easy.txt"));
 		
 		KiteDemo kiteDemo = new KiteDemo();
 		kiteDemo.onStart();
