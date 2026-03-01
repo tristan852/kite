4c7bb774bc283d3b19822ccb533fd1835f5e07f2
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-03-01 10:02:43 +0100

    Update Board.java

--- src/main/java/net/kite/internal/board/Board.java
+++ src/main/java/net/kite/internal/board/Board.java
@@ -1588,6 +1588,7 @@ public final class Board {
 		if(outcomeStayedTheSame) {
 			
 			int scoreLoss = scoreBefore - scoreAfter;
+			if(scoreLoss > 9) return MoveAnalysis.MoveQuality.BLUNDER;
 			if(scoreLoss > 6) return scoreAfter < 0 ? MoveAnalysis.MoveQuality.BLUNDER : MoveAnalysis.MoveQuality.MISTAKE;
 			
 			boolean scoreDroppedALot = scoreLoss > 3;
