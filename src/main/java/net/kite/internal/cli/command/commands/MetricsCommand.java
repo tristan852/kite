061734e29be8bdc39d8f738c620c75cb7e82289f
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-03-05 21:30:05 +0100

    Create MetricsCommand.java

---
+++ src/main/java/net/kite/internal/cli/command/commands/MetricsCommand.java
@@ -0,0 +1,66 @@
+package net.kite.internal.cli.command.commands;
+
+import net.kite.api.Kite;
+import net.kite.internal.cli.command.Command;
+import net.kite.internal.util.ansi.AnsiUtil;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public final class MetricsCommand extends Command {
+	
+	public MetricsCommand() {
+		super("metrics", "record", "Start and stop recording metrics", "metrics [reset]");
+	}
+	
+	@Override
+	public boolean execute(String[] arguments, Kite solver, PrintStream errorStream, boolean exitOnError, boolean quiet, Scanner scanner) {
+		boolean reset = false;
+		
+		if(arguments.length == 1) {
+			
+			String s = arguments[0];
+			
+			if(s.equals("true")) reset = true;
+			else if(s.equals("false")) reset = false;
+			else {
+				
+				errorStream.println(AnsiUtil.brightRedAnsi(String.format("Unknown boolean value for argument 'reset': \"%s\"", s)));
+				if(exitOnError) System.exit(1);
+				return false;
+			}
+			
+		} else if(arguments.length > 1) {
+			
+			errorStream.println(AnsiUtil.brightRedAnsi("Too many arguments!"));
+			if(exitOnError) System.exit(1);
+			return false;
+		}
+		
+		if(solver.isRecordingMetrics()) {
+			
+			solver.stopRecordingPerformanceMetrics();
+			System.out.println("Recording stopped...");
+			
+			if(reset) {
+				
+				System.out.println();
+				solver.printAndResetPerformanceMetrics();
+			}
+			
+		} else {
+			
+			if(reset) {
+				
+				solver.printAndResetPerformanceMetrics();
+				System.out.println();
+			}
+			
+			solver.startRecordingPerformanceMetrics();
+			System.out.println("Recording started...");
+		}
+		
+		return false;
+	}
+	
+}
