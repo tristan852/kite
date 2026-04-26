27aca20c3040884e6b449f6f771b2be27909aeef
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-04-26 18:24:24 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,9 @@ module kite.main {
 	exports net.kite.api.board.evaluation;
 	exports net.kite.api.skill.level;
 	
+	requires it.unimi.dsi.sux4j;
+	requires it.unimi.dsi.fastutil;
+	requires it.unimi.dsi.dsiutils;
 	requires org.fusesource.jansi;
 	requires org.teavm.jso.apis;
 }
