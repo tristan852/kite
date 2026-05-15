454afd730e534ad0088b9e80d053dfda1777dd87
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-05-15 11:14:36 +0200

    Create AdaptiveCommand.java

---
+++ src/main/java/net/kite/internal/cli/command/commands/AdaptiveCommand.java
@@ -0,0 +1,38 @@
+package net.kite.internal.cli.command.commands;
+
+import net.kite.api.Kite;
+import net.kite.internal.cli.command.Command;
+import net.kite.internal.util.ansi.AnsiUtil;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public final class AdaptiveCommand extends Command {
+	
+	public AdaptiveCommand() {
+		super("adaptive", "ad", "Show a move by the adaptive skill level", "adaptive");
+	}
+	
+	@Override
+	public boolean execute(String[] arguments, Kite solver, PrintStream errorStream, boolean exitOnError, boolean quiet, Scanner scanner) {
+		if(arguments.length != 0) {
+			
+			errorStream.println(AnsiUtil.brightRedAnsi("Too many arguments!"));
+			if(exitOnError) System.exit(1);
+			return false;
+		}
+		
+		int move = solver.adaptiveMove();
+		if(move == 0) {
+			
+			errorStream.println(AnsiUtil.brightRedAnsi("The game is over!"));
+			if(exitOnError) System.exit(1);
+			return false;
+		}
+		
+		System.out.println(move);
+		
+		return false;
+	}
+	
+}
