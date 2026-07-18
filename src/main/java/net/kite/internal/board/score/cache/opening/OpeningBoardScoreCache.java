0b4e8b4e0f3570c47e5bf7e08b207f573e54f4e2
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-07-18 18:04:52 +0200

    Update OpeningBoardScoreCache.java

--- src/main/java/net/kite/internal/board/score/cache/opening/OpeningBoardScoreCache.java
+++ src/main/java/net/kite/internal/board/score/cache/opening/OpeningBoardScoreCache.java
@@ -2,25 +2,35 @@ package net.kite.internal.board.score.cache.opening;
 
 import net.kite.internal.board.score.cache.BoardScoreCache;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public final class OpeningBoardScoreCache {
 	
-	private static final int PACKED_BOARD_SCORES_SIZE_IN_BYTES = 72397224;
-	private static final int HASH_FUNCTION_SIZE_IN_BYTES       = 27134232;
+	private static final int PACKED_BOARD_SCORES_SIZE_IN_BYTES = 61737771;
+	private static final int BOARD_SCORES_SIZE                 = 82317028;
+	private static final int BUCKET_SEEDS_SIZE_IN_BYTES        = 33554432;
+	private static final int BUCKET_SEEDS_INDEX_MASK           = 33554431;
 	
-	private static final int BOARD_SCORE_SIZE_IN_BITS = 6;
 	private static final int BOARD_SCORE_MASK = 0x3F;
 	private static final int BOARD_SCORE_OFFSET = -18;
+	
+	private static final long MIXED_LONG_ADDEND            = 0x9E3779B97F4A7C15L;
+	private static final long MIXED_LONG_FIRST_MULTIPLIER  = 0xBF58476D1CE4E5B9L;
+	private static final long MIXED_LONG_SECOND_MULTIPLIER = 0x94D049BB133111EBL;
+	
+	private static final int MIXED_LONG_FIRST_SHIFT_AMOUNT  = 30;
+	private static final int MIXED_LONG_SECOND_SHIFT_AMOUNT = 27;
+	private static final int MIXED_LONG_THIRD_SHIFT_AMOUNT  = 31;
 	
 	private static final int BYTE_MASK = 0xFF;
 	
 	private final byte[] packedBoardScores;
+	private final byte[] bucketSeeds;
 	
 	public OpeningBoardScoreCache() {
 		this.packedBoardScores = new byte[PACKED_BOARD_SCORES_SIZE_IN_BYTES];
+		this.bucketSeeds = new byte[BUCKET_SEEDS_SIZE_IN_BYTES];
 	}
 	
 	public void loadFromResources(String resourcePath) {
@@ -34,6 +44,7 @@ public final class OpeningBoardScoreCache {
 		
 		try(inputStream) {
 			
+			inputStream.readNBytes(bucketSeeds, 0, BUCKET_SEEDS_SIZE_IN_BYTES);
 			inputStream.readNBytes(packedBoardScores, 0, PACKED_BOARD_SCORES_SIZE_IN_BYTES);
 			
 		} catch(IOException exception) {
@@ -52,8 +63,8 @@ public final class OpeningBoardScoreCache {
 		
 		try {
 			
-			System.arraycopy(bytes, 0, packedBoardScores, 0, PACKED_BOARD_SCORES_SIZE_IN_BYTES);
-			InputStream stream = new ByteArrayInputStream(bytes, PACKED_BOARD_SCORES_SIZE_IN_BYTES, HASH_FUNCTION_SIZE_IN_BYTES);
+			System.arraycopy(bytes, 0, bucketSeeds, 0, BUCKET_SEEDS_SIZE_IN_BYTES);
+			System.arraycopy(bytes, BUCKET_SEEDS_SIZE_IN_BYTES, packedBoardScores, 0, PACKED_BOARD_SCORES_SIZE_IN_BYTES);
 			
 		} catch(Exception exception) {
 			
@@ -62,10 +73,11 @@ public final class OpeningBoardScoreCache {
 		}
 	}
 	
-	// TODO implement
 	public int boardScore(long mixedHash) {
-		int index = 0;
-		index *= BOARD_SCORE_SIZE_IN_BITS;
+		int bucketIndex = (int) mixedHash & (BUCKET_SEEDS_INDEX_MASK);
+		byte bucketSeed = bucketSeeds[bucketIndex];
+		
+		int index = (int) Long.remainderUnsigned(mixLong(mixedHash, bucketSeed), BOARD_SCORES_SIZE);
 		
 		int byteIndex = index >> 3;
 		int bitIndex = index - (byteIndex << 3);
@@ -76,6 +88,24 @@ public final class OpeningBoardScoreCache {
 		i &= BOARD_SCORE_MASK;
 		
 		return i + BOARD_SCORE_OFFSET;
+	}
+	
+	private static long mixLong(long l, byte seed) {
+		long s = ((long) seed) & BYTE_MASK;
+		
+		s = mixLong(s);
+		l ^= s;
+		
+		return mixLong(l);
+	}
+	
+	private static long mixLong(long l) {
+		l += MIXED_LONG_ADDEND;
+		
+		l = (l ^ (l >>> MIXED_LONG_FIRST_SHIFT_AMOUNT)) * MIXED_LONG_FIRST_MULTIPLIER;
+		l = (l ^ (l >>> MIXED_LONG_SECOND_SHIFT_AMOUNT)) * MIXED_LONG_SECOND_MULTIPLIER;
+		
+		return l ^ (l >>> MIXED_LONG_THIRD_SHIFT_AMOUNT);
 	}
 	
 }
