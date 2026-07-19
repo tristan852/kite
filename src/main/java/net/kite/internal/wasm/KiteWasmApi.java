c7be09ea62eb696fd6d710bd806cc7a06e8eeed5
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-07-19 14:40:27 +0200

    Create KiteWasmApi.java

---
+++ src/main/java/net/kite/internal/wasm/KiteWasmApi.java
@@ -0,0 +1,19 @@
+package net.kite.internal.wasm;
+
+import net.kite.api.Kite;
+import org.teavm.jso.JSExport;
+
+public class KiteWasmApi {
+	
+	private static Kite solver;
+	
+	public static void initialize() {
+		
+	}
+	
+	@JSExport
+	public static void debug() {
+		
+	}
+	
+}
