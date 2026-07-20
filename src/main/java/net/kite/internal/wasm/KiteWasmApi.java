dae8dcf628047442af82b1c0beea8b83410ada1a
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-07-20 10:36:42 +0200

    Update KiteWasmApi.java

--- src/main/java/net/kite/internal/wasm/KiteWasmApi.java
+++ src/main/java/net/kite/internal/wasm/KiteWasmApi.java
@@ -8,12 +8,21 @@ public class KiteWasmApi {
 	private static Kite solver;
 	
 	public static void initialize() {
-		
+		System.out.println("main method called!");
+		System.out.println("Should load everything here!");
 	}
 	
 	@JSExport
 	public static int debug() {
 		return 1234;
 	}
+	
+	// @Export(name = "create")
+	//@Export(name = "solve")
+	//@Export(name = "getBestMove")
+	//@Export(name = "evaluate")
+	//@Export(name = "getLegalMoves")
+	//@Export(name = "isGameOver")
+	//@Export(name = "analyze")
 	
 }
